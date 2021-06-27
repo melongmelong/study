c1966aa6f81022aab725fe54980ffbe9b2e5ea6b
knw <knw>
2021-06-27 16:23:32 +0900

    update load_linux

--- toy/load_linux/main.c
+++ toy/load_linux/main.c
@@ -1,4 +1,19 @@
+extern char rom_start;
+extern char rom_end;
+
 int main(void)
 {
+	char *p = (char*)&rom_start;
+	char *addr = (char*)0x20000000;
+
+	
+	while((int)p<(int)rom_end){
+		*addr = *p;
+		if(*addr == '\n'){
+			return 0xff;
+		}
+		addr++;
+		p++;
+	}
 	return 0x78;
 }
--- toy/load_linux/run.sh
+++ toy/load_linux/run.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-qemu-system-x86_64 -bios ./img.bin -monitor stdio
+qemu-system-x86_64 -m 4g -bios ./img.bin -monitor stdio
