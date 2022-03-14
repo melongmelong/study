f716766cdace091beab23e8b6f896fc07d2fa1b8
knw <knw>
2022-03-14 20:44:56 +0900

    leetcode 59, new

--- PS/leetcode/59.c
+++ PS/leetcode/59.c
@@ -1,28 +1,4 @@
 
-void fill(int **arr, int n, int sx, int sy, int snum, int *nx, int *ny, int *nnum){
-    int i;
-    
-    for(i=0; i<n; i++){
-        arr[sy][i+sx] = snum;
-        snum++;
-    }
-    for(i=0; i<n-1; i++){
-        arr[i+sy+1][sx+n-1] = snum;
-        snum++;
-    }
-    for(i=0; i<n-1; i++){
-        arr[sy+n-1][sx+n-2-i] = snum;
-        snum++;
-    }
-    for(i=0; i<n-2; i++){
-        arr[sy+n-2-i][sx] = snum;
-        snum++;
-    }
-    
-    *nx = sx+1;
-    *ny = sy+1;
-    *nnum = snum;
-}
 
 /**
  * Return an array of arrays of size *returnSize.
@@ -30,13 +6,16 @@ void fill(int **arr, int n, int sx, int sy, int snum, int *nx, int *ny, int *nnu
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 int** generateMatrix(int n, int* returnSize, int** returnColumnSizes){
-    int nx, ny, nnum;
-    int i;
+    int tbl[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+    int state = 0;
+    int num;
+    int r, c, i;
     int **ans;
-    
+   
     ans = (int**)malloc(sizeof(int*) * n);
     for(i=0; i<n; i++){
         ans[i] = (int*)malloc(sizeof(int) * n);
+        memset(ans[i], 0, sizeof(int) * n);
     }
     
     *returnColumnSizes = (int*)malloc(sizeof(int) * n);
@@ -45,12 +24,21 @@ int** generateMatrix(int n, int* returnSize, int** returnColumnSizes){
     }
     
     *returnSize = n;
-   
-    nx = ny = 0;
-    nnum = 1;
-    while(n > 0){
-        fill(ans, n, nx, ny, nnum, &nx, &ny, &nnum);
-        n -= 2;
+  
+    r = c = 0;
+    num = 1;
+    while(num <= n*n){
+        ans[r][c] = num;
+        
+        if(r+tbl[state][0] >= n || c+tbl[state][1] >= n || r+tbl[state][0] < 0 || c+tbl[state][1] < 0 
+           || ans[r+tbl[state][0]][c+tbl[state][1]] != 0){
+            state = (state + 1) % 4;
+        }
+        
+        r = r + tbl[state][0];
+        c = c + tbl[state][1];
+        
+        num++;
     }
     
     return ans;
