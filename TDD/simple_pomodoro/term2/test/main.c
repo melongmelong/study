a12fee72ba3b0aa5e55a0ee1c3caafd76af142c7
knw <knw>
2023-12-14 20:57:42 +0900

    Add term2 directory
    Add term2 directory for second TDD challenge.
    This directory include basic source, script based on term dir.

---
+++ TDD/simple_pomodoro/term2/test/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "CUnit/Basic.h"
+#include "CUnit/Console.h"
+
+int main(int argc, char **argv)
+{
+	CU_pSuite test_suite = NULL;
+
+	CU_initialize_registry();
+
+	test_suite = CU_add_suite("test_suite", NULL, NULL);
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+
+	CU_cleanup_registry();
+	return 0;
+}
