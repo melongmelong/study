fb04dde66ab3bf893a696cfd330a212ec477f43b
knw <knw>
2024-12-09 21:16:58 +0900

    Added multi-process architecture.
    Added multi-process server feature with refactoring.

--- linux/application_programming/termprj/src/server_main.c
+++ linux/application_programming/termprj/src/server_main.c
@@ -1,42 +1,49 @@
 #include "server.h"
 
-int main(int argc, char **argv)
+static struct context_conn* do_accept(struct context_server *context_server)
 {
-	char *comm = NULL, *ip = NULL, *port = NULL;
-	struct context_server *context_server = NULL;
-	struct transport transport = {
-		.socket = socket,
-		.bind = bind,
-		.listen = listen,
-		.accept = accept,
-		.close = close,
-		.read = read,
-		.write = write
-	};
 	struct context_conn *context_conn = NULL;
-	char read_data[1024] = "";
-	int ret = 0;
 
-	comm = argv[0];
-
-	if (argc != 3) {
-		printf("%s [ip] [port]\n", argv[0]);
-		return -1;
+	if (context_server == NULL) {
+		return NULL;
 	}
 
-	ip = argv[1];
-	port = argv[2];
-
-	server_init_signal();
-
-	context_server = server_init(ip, atoi(port), &transport);
+	printf("wait accept(pid:%d)\n", getpid());
 
 	do {
 		context_conn = server_accept(context_server);
+		if (is_server_exit) {
+			printf("sigint recieved!(pid:%d)\n", getpid());
+			context_conn = NULL;
+			break;
+		}
+
 	} while ((int)context_conn == -1 && errno == EINTR);
+
+	return context_conn;
+}
+
+static void do_service(struct context_conn *context_conn)
+{
+	struct context_server *context_server = NULL;
+	char read_data[1024] = "";
+	int ret = 0;
+
+	if (context_conn == NULL) {
+		return;
+	}
+
+	context_server = context_conn->context_server;
+
+	// reinit signal just for re-set alarm()!
+	// it seems too much... :)
+	server_init_signal();
+
+	printf("client acceptd(pid:%d, accepted_sock:%d, cnt_context_conn:%d)!\n", getpid(), context_conn->sock, context_server->cnt_context_conn);
 
 	while (1) {
 		if (is_server_exit) {
+			printf("sigint recieved!(pid:%d)\n", getpid());
 			break;
 		}
 
@@ -44,7 +51,7 @@ int main(int argc, char **argv)
 			is_server_alarm = 0;
 
 			if (server_check_conn_timeout(context_server, context_conn)) {
-				printf("client timeout!\n");
+				printf("client timeout!(pid:%d)\n", getpid());
 				break;
 			}
 		}
@@ -63,16 +70,66 @@ int main(int argc, char **argv)
 			if (ret < 0 && errno == EINTR)	continue;
 			else	break;
 		}
-		printf("read(%s)\n", read_data);
+		printf("read-pid:%d(%s)\n", getpid(), read_data);
 		if ((ret = server_write(context_server, context_conn, read_data, strlen(read_data) + 1)) <= 0) {
 			if (ret < 0 && errno == EINTR)	continue;
 			else	break;
 		}
 	}
-
-	printf("server close!\n");
-
+			
 	server_close(context_server, context_conn);
+}
+
+int main(int argc, char **argv)
+{
+	char *comm = NULL, *ip = NULL, *port = NULL;
+	struct context_server *context_server = NULL;
+	struct transport transport = {
+		.socket = socket,
+		.bind = bind,
+		.listen = listen,
+		.accept = accept,
+		.close = close,
+		.read = read,
+		.write = write
+	};
+	struct context_conn *context_conn = NULL;
+	pid_t pid;
+
+	comm = argv[0];
+
+	if (argc != 3) {
+		printf("%s [ip] [port]\n", argv[0]);
+		return -1;
+	}
+
+	ip = argv[1];
+	port = argv[2];
+
+	printf("parent pid:%d, ip:%s, port:%s\n", getpid(), ip, port);
+
+	server_init_signal();
+
+	context_server = server_init(ip, atoi(port), &transport);
+
+	while (1) {
+		context_conn = do_accept(context_server);
+		if (context_conn == NULL) {
+			if (is_server_exit) break;
+			else continue;
+		}
+
+		pid = fork();
+		if (pid == 0) {
+			do_service(context_conn);
+			break;
+		}
+		else {
+			server_close(context_server, context_conn);
+		}
+	}
+
+	printf("server close(pid:%d)!\n", getpid());
 
 	server_deinit(&context_server);
 
