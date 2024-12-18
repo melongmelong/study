ede07a69c2f7a64dd2e391767ae26efbc7851d84
knw <knw>
2024-12-18 21:57:16 +0900

    Added threaded server.
    Added threaded server version.
    Added option - select server type(process version or thread version)

--- linux/application_programming/termprj/src/server_main.c
+++ linux/application_programming/termprj/src/server_main.c
@@ -1,3 +1,5 @@
+#include <pthread.h>
+
 #include "server.h"
 
 static struct context_conn* do_accept(struct context_server *context_server)
@@ -76,41 +78,17 @@ static void do_service(struct context_conn *context_conn)
 			else	break;
 		}
 	}
-			
+
+	printf("connection closed(pid:%ld)\n", getpid());
 	server_close(context_server, context_conn);
 }
 
-int main(int argc, char **argv)
+static void process_main(struct context_server *context_server)
 {
-	char *comm = NULL, *ip = NULL, *port = NULL;
-	struct context_server *context_server = NULL;
-	struct transport transport = {
-		.socket = socket,
-		.bind = bind,
-		.listen = listen,
-		.accept = accept,
-		.close = close,
-		.read = read,
-		.write = write
-	};
+	pid_t pid; 
 	struct context_conn *context_conn = NULL;
-	pid_t pid;
-
-	comm = argv[0];
-
-	if (argc != 3) {
-		printf("%s [ip] [port]\n", argv[0]);
-		return -1;
-	}
-
-	ip = argv[1];
-	port = argv[2];
-
-	printf("parent pid:%d, ip:%s, port:%s\n", getpid(), ip, port);
 
 	server_init_signal();
-
-	context_server = server_init(ip, atoi(port), &transport);
 
 	while (1) {
 		context_conn = do_accept(context_server);
@@ -129,11 +107,266 @@ int main(int argc, char **argv)
 		}
 	}
 
+	server_deinit_signal();
+}
+
+static struct thread_info {
+	pthread_t tid;
+	char valid;
+	struct context_conn *context_conn;
+	int is_server_exit, is_server_alarm;
+} thread_info[MAX_CONTEXT_CONN];
+
+static struct thread_info* thread_info_find_empty(void)
+{
+	int i = 0;
+
+	for (i = 0; i < MAX_CONTEXT_CONN; i++) {
+		if (thread_info[i].valid == 0) {
+			return &thread_info[i];
+		}
+	}
+
+	return NULL;
+}
+
+static struct thread_info* thread_info_find(pthread_t tid)
+{
+	int i = 0;
+
+	for (i = 0; i < MAX_CONTEXT_CONN; i++) {
+		if (thread_info[i].valid && thread_info[i].tid == tid) {
+			return &thread_info[i];
+		}
+	}
+
+	return NULL;
+}
+
+static void thread_info_broadcast_signal(int signo)
+{
+	int i = 0;
+
+	for (i = 0; i < MAX_CONTEXT_CONN; i++) {
+		if (thread_info[i].valid && thread_info[i].tid != pthread_self()) {
+			pthread_kill(thread_info[i].tid, signo);
+		}
+	}
+}
+
+static void sigint_thread(int signo)
+{
+	int i = 0;
+
+	for (i = 0; i < MAX_CONTEXT_CONN; i++) {
+		if (thread_info[i].valid) {
+			thread_info[i].is_server_exit = 1;
+		}
+	}
+
+	thread_info_broadcast_signal(SIGUSR1);
+}
+
+static void sigalarm_thread(int signo)
+{
+	int i = 0;
+
+	for (i = 0; i < MAX_CONTEXT_CONN; i++) {
+		if (thread_info[i].valid) {
+			thread_info[i].is_server_alarm = 1;
+		}
+	}
+
+	thread_info_broadcast_signal(SIGUSR1);
+	alarm(1);
+}
+
+static void sigusr_thread(int signo)
+{
+	//no-op
+}
+
+static void init_signal_thread(void)
+{
+	struct sigaction act;
+
+	act.sa_handler = sigint_thread;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	sigaction(SIGINT, &act, NULL);
+
+	act.sa_handler = sigusr_thread;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	sigaction(SIGUSR1, &act, NULL);
+
+	act.sa_handler = sigalarm_thread;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	sigaction(SIGALRM, &act, NULL);
+	alarm(1);
+}
+
+static struct context_conn* do_accept_thread(struct context_server *context_server)
+{
+	struct thread_info *thread_info = thread_info_find(pthread_self());
+	struct context_conn *context_conn = NULL;
+
+	if (context_server == NULL) {
+		return NULL;
+	}
+
+	printf("wait accept(tid:%ld)\n", pthread_self());
+
+	do {
+		context_conn = server_accept(context_server);
+		if (thread_info->is_server_exit) {
+			printf("sigint recieved!(pid:%d)\n", getpid());
+			context_conn = NULL;
+			break;
+		}
+
+		if (thread_info->is_server_alarm) {
+			thread_info->is_server_alarm = 0;
+		}
+		
+	} while ((int)context_conn == -1 && errno == EINTR);
+
+	return context_conn;
+}
+
+static void* do_service_thread(struct thread_info *thread_info)
+{
+	struct context_server *context_server = NULL;
+	struct context_conn *context_conn = NULL;
+	char read_data[1024] = "";
+	int ret = 0;
+
+	context_conn = thread_info->context_conn;
+	context_server = context_conn->context_server;
+
+	if (context_conn == NULL) {
+		return NULL;
+	}
+
+	printf("client acceptd(tid:%ld, accepted_sock:%d, cnt_context_conn:%d)!\n", pthread_self(), context_conn->sock, context_server->cnt_context_conn);
+
+	while (1) {
+		if (thread_info->is_server_exit) {
+			printf("sigint recieved!(tid:%ld)\n", pthread_self());
+			break;
+		}
+
+		if (thread_info->is_server_alarm) {
+			thread_info->is_server_alarm = 0;
+
+			if (server_check_conn_timeout(context_server, context_conn)) {
+				printf("client timeout!(tid:%ld)\n", pthread_self());
+				break;
+			}
+		}
+
+		/*
+		TODO : need to consider more...
+			broadcast msg is interleaved, so echo msg is delayed...
+		if (is_server_broadcasting) {
+			is_server_broadcasting = 0;
+			server_broadcast(context_server);
+			printf("send braodcast!\n");
+		}
+		*/
+
+		if ((ret = server_read(context_server, context_conn, read_data, sizeof(read_data))) <= 0) {
+			if (ret < 0 && errno == EINTR)	continue;
+			else	break;
+		}
+		printf("read-tid:%ld(%s)\n", pthread_self(), read_data);
+		if ((ret = server_write(context_server, context_conn, read_data, strlen(read_data) + 1)) <= 0) {
+			if (ret < 0 && errno == EINTR)	continue;
+			else	break;
+		}
+	}
+			
+	printf("connection closed(tid:%ld)\n", pthread_self());			
+	server_close(context_server, context_conn);
+	thread_info->valid = 0;
+
+	return NULL;
+}
+
+static void thread_main(struct context_server *context_server)
+{
+	pthread_t pthread;
+	struct thread_info *thread_info = NULL;
+	struct context_conn *context_conn = NULL;
+
+	// set up for listening thread.
+	thread_info = thread_info_find_empty();
+	thread_info->tid = pthread_self();
+	thread_info->valid = 1;
+	thread_info->context_conn = NULL;
+
+	// use other signal handler on thread version.
+	init_signal_thread();
+
+	while (1) {
+		context_conn = do_accept_thread(context_server);
+		if (context_conn == NULL) {
+			thread_info = thread_info_find(pthread_self());
+			if (thread_info->is_server_exit) break;
+			else continue;
+		}
+
+		thread_info = thread_info_find_empty();
+		thread_info->valid = 1;
+		thread_info->context_conn = context_conn;
+
+		pthread_create(&thread_info->tid, NULL, do_service_thread, thread_info);
+		pthread_detach(thread_info->tid);
+	}
+
 	printf("server close(pid:%d)!\n", getpid());
+}
+
+int main(int argc, char **argv)
+{
+	char *comm = NULL, *ip = NULL, *port = NULL, *type = NULL;
+	struct context_server *context_server = NULL;
+	struct transport transport = {
+		.socket = socket,
+		.bind = bind,
+		.listen = listen,
+		.accept = accept,
+		.close = close,
+		.read = read,
+		.write = write
+	};
+	
+	comm = argv[0];
+
+	if (argc != 4) {
+		printf("%s [ip] [port] [type]\n", argv[0]);
+		return -1;
+	}
+
+	ip = argv[1];
+	port = argv[2];
+	type = argv[3];
+
+	printf("parent pid:%d, ip:%s, port:%s, type:%s\n", getpid(), ip, port, type);
+
+	context_server = server_init(ip, atoi(port), &transport);
+
+	switch (atoi(type)) {
+	case 0:
+		process_main(context_server);
+		break;
+	case 1:
+		thread_main(context_server);
+		break;
+	}
 
 	server_deinit(&context_server);
-
-	server_deinit_signal();
 
 	return 0;
 }
